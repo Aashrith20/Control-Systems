6b1b27381ca957dd069b2d25260cafb167d835c2
Aashrith20 <61198337+Aashrith20@users.noreply.github.com>
2020-04-21 16:55:54 +0530

    Update ee18btech11035_1.py

--- manual/codes/ee18btech11035_1.py
+++ manual/codes/ee18btech11035_1.py
@@ -1,19 +1,23 @@
 import numpy as np
 import matplotlib.pyplot as plt 
 
-t=np.linspace(-3,100,1000)
-undamped=2*np.cos(t)*np.heaviside(t,1)
-underdamped=2*np.exp(-(0.05)*t)*np.cos(t)*np.heaviside(t,1)
-critical_damped=(np.exp(-0.05*t)+np.exp(-0.05*t))*np.heaviside(t,1)
-overdamped=(np.exp(-0.05*t)+np.exp(-0.03*t))*np.heaviside(t,1)
+t=np.linspace(-3,15,1000)
+y1=(1-np.exp(-t)-t*np.exp(-t))*np.heaviside(t,1)
+plt.plot(t,y1,label='$y(t)=(1-e^{t}-te^{t})u(t)$')
 
-plt.plot(t,undamped,label='$h_1(t) = Undamped(\zeta = 0)$')
-plt.plot(t,underdamped,label='$h_2(t) = Underdamped(0<\zeta<1)$')
-plt.plot(t,critical_damped,label='$h_3(t) = Critical damped(\zeta = 1)$')
-plt.plot(t,overdamped,label='$h_4(t) = Overdamped(\zeta>1)$')
+
+x2=np.linspace(-3,5.834,1000)
+y2=0.98*np.ones(1000)
+plt.plot(x2,y2,'--',label='$y(t)=0.98$')
+
+
+x3=5.834*np.ones(1000)
+y3=np.linspace(0,0.98,1000)
+
+plt.plot(x3,y3,'--',label='$t=5.834sec$')
+plt.text(5.834,0.98,'(5.834,0.98)',horizontalalignment='right')
 plt.grid()
 plt.xlabel('time(sec)')
-plt.ylabel('h(t)')
+plt.ylabel('step response y(t)')
 plt.legend()
 plt.show()
-

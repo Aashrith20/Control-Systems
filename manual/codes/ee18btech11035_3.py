ed294bdfe89f1c3e53e46bb699d04006c67afa2e
Aashrith20 <61198337+Aashrith20@users.noreply.github.com>
2020-04-21 16:57:08 +0530

    Update ee18btech11035_3.py

--- manual/codes/ee18btech11035_3.py
+++ manual/codes/ee18btech11035_3.py
@@ -1,11 +1,20 @@
+from mpl_toolkits.mplot3d import axes3d
+import matplotlib.pyplot as plt
 import numpy as np
-import matplotlib.pyplot as plt 
 
-t=np.linspace(-3,10,1000)
-f=(1-np.exp(-t)-t*np.exp(-t))*np.heaviside(t,1)
-plt.plot(t,f,label='$y(t) = 1-e^{-t}-te^{-t}$')
-plt.grid()
-plt.xlabel('time(sec)')
-plt.ylabel('y(t)')
-plt.legend()
+
+x = np.arange(0,10,0.01)
+y = np.arange(0,0.99,0.001)
+t,k = np.meshgrid(x,y)
+y = 1-((np.exp(-t))*(((np.sinh(t*np.sqrt(1-k)))/(np.sqrt(1-k)))+(np.cosh(t*np.sqrt(1-k)))))
+
+fig = plt.figure(figsize=(6,6))
+ax = fig.add_subplot(111, projection='3d')
+
+# Plot a 3D surface
+ax.plot_surface(t, k, y)
+ax.set_xlabel('time(sec)')
+ax.set_ylabel('k')
+ax.set_zlabel('y(t)')
+
 plt.show()
